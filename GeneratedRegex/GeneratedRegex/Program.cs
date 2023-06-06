0c3250c41af930732c89391489e37f0947b68311
Paweł Bulwan <github@taurit.pl>
2023-06-06 18:27:19 +0200

    Add example of usage of GeneratedRegex attribute

---
+++ GeneratedRegex/GeneratedRegex/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System.Text.RegularExpressions;
+
+var summary = BenchmarkRunner.Run<MyBenchmark>();
+Console.WriteLine(summary);
+
+public partial class MyBenchmark
+{
+    private const string SampleInput = "   input  with     too many whitespaces   ";
+
+    [Benchmark]
+    public string OldImplementation() => OldImplementation(SampleInput);
+
+    [Benchmark]
+    public string NewImplementation() => NewImplementation(SampleInput);
+
+    // OLD IMPLEMENTATION
+    private static readonly Regex MultipleWhitespacesRegex = new(@"\s+");
+    private string OldImplementation(string input) => MultipleWhitespacesRegex.Replace(input, " ");
+
+    // NEW IMPLEMENTATION
+    [GeneratedRegex(@"\s+")]
+    private static partial Regex MultipleWhitespacesGeneratedRegex();
+    private string NewImplementation(string input) => MultipleWhitespacesGeneratedRegex().Replace(input, " ");
+}
