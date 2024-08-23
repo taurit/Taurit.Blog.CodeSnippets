0ee4af7828a7782464a99d7d9bbd98339ea33c76
Paweł Bulwan <github@taurit.pl>
2024-08-23 11:29:07 +0200

    Add code sample for the "Structured Outputs" blog article

---
+++ OpenAiGenerateJsonSchemaFromClass/GenerateJsonSchema/Country.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace GenerateJsonSchema;
+
+// Declare the format of the GPT model's response as a C# class
+public class Country
+{
+    [Description("Country name preferred in English language")]
+    public string InternationalName { get; set; }
+
+    public int EstimatedPopulation { get; set; }
+
+    [Description("Names of 3 people from that country most recognizable abroad")]
+    public List<string> FamousPeopleExample { get; set; }
+}
---
+++ OpenAiGenerateJsonSchemaFromClass/GenerateJsonSchema/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Schema;
+using Newtonsoft.Json.Schema.Generation;
+using OpenAI;
+using OpenAI.Managers;
+using OpenAI.ObjectModels.RequestModels;
+using Spectre.Console;
+using Spectre.Console.Json;
+
+namespace GenerateJsonSchema;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Prepare the JSON schema
+        JSchemaGenerator generator = new JSchemaGenerator();
+        generator.DefaultRequired = Required.Always; // required by OpenAI
+
+        JSchema schema = generator.Generate(typeof(Country));
+        schema.AllowAdditionalProperties = false; // required by OpenAI
+
+        var schemaAsString = schema.ToString();
+        AnsiConsole.Write(new JsonText(schemaAsString));
+
+        return;
+
+        // Send query to OpenAI
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        var openAiDeveloperKey = config["OpenAiDeveloperKey"]!;
+        var openAiOrganizationId = config["OpenAiOrganization"]!;
+
+        var openAiService = new OpenAIService(new OpenAiOptions()
+        {
+            ApiKey = openAiDeveloperKey,
+            Organization = openAiOrganizationId,
+            DefaultModelId = "gpt-4o-2024-08-06"
+        });
+
+        var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+        {
+            Messages = new List<ChatMessage>
+            {
+                ChatMessage.FromSystem("You are a helpful assistant."),
+                ChatMessage.FromUser("Provide information about Poland"),
+            },
+            // TODO - use Structured Outputs when it's available in the library (ticket: https://github.com/betalgo/openai/issues/617)
+        });
+
+        if (completionResult.Successful)
+        {
+            var responseText = completionResult.Choices.First().Message.Content;
+            AnsiConsole.Write(new JsonText(responseText));
+        }
+        else
+        {
+            AnsiConsole.MarkupLine($"[red]Error:[/] {completionResult.Error?.Message}");
+        }
+
+
+    }
+}
