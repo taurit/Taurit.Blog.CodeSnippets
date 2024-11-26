5cb7211f6c9ac192e62598272be4be5b761a0407
Paweł Bulwan <github@taurit.pl>
2024-11-26 11:41:04 +0100

    Add demo: .NET 9 app referencing .NET 8 library with NET8->9 breaking changes

---
+++ Dotnet8And9CombinationTest/Dotnet8Library/Dotnet8Helper.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Dotnet8Library;
+
+public static class Dotnet8Helper
+{
+    public static int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void UseBinaryFormatter()
+    {
+#pragma warning disable SYSLIB0011
+        var serializer = new BinaryFormatter();
+#pragma warning restore SYSLIB0011
+        var stream = new MemoryStream();
+        var obj = new object();
+        serializer.Serialize(stream, obj);
+    }
+}
---
+++ Dotnet8And9CombinationTest/Dotnet9ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Dotnet9ConsoleApp;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        try
+        {
+            Console.WriteLine("# Calling a compatible method");
+            Console.WriteLine("Dotnet8Library.Dotnet8Helper.Add(1, 2): " + Dotnet8Library.Dotnet8Helper.Add(1, 2));
+            Console.WriteLine("");
+
+            Console.WriteLine("# Calling an incompatible method");
+            Dotnet8Library.Dotnet8Helper.UseBinaryFormatter();
+
+            Console.WriteLine("Done (no exceptions observed)");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Exception: {ex.Message}");
+        }
+    }
+}
