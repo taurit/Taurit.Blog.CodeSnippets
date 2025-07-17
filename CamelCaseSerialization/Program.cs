1e2753d89cbfa34783d9c836dfb52a1a444f9252
Paweł Bulwan <github@taurit.pl>
2025-07-17 18:41:48 +0200

    Pivot the table

--- CamelCaseSerialization/Program.cs
+++ CamelCaseSerialization/Program.cs
@@ -1,11 +1,8 @@
-﻿namespace CamelCaseSerialization;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
+using JsonSerializer = System.Text.Json.JsonSerializer;
 
 public class TestObject
 {
@@ -23,118 +20,155 @@ class Program
         var testObject = new TestObject();
         var results = new Dictionary<string, Dictionary<string, string>>();
         var propertyNames = typeof(TestObject).GetProperties().Select(p => p.Name).ToList();
-
-        // Initialize results dictionary
-        foreach (var prop in propertyNames)
-        {
-            results[prop] = new Dictionary<string, string>();
-        }
+        var serializers = new List<string>();
 
         // Newtonsoft.Json (Default)
+        var serializerName = "Newtonsoft.Json (Default)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var defaultJson = JsonConvert.SerializeObject(testObject);
         var defaultParsed = JObject.Parse(defaultJson);
         foreach (var prop in defaultParsed.Properties())
         {
-            results[prop.Name]["Newtonsoft.Json (Default)"] = prop.Name;
+            var originalProp = propertyNames.FirstOrDefault(p => testObject.GetType().GetProperty(p).GetValue(testObject).ToString() == prop.Value.ToString());
+            if (originalProp != null)
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // Newtonsoft.Json (CamelCase)
+        serializerName = "Newtonsoft.Json (CamelCase)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var camelCaseSettings = new JsonSerializerSettings
         {
             ContractResolver = new DefaultContractResolver { NamingStrategy = new CamelCaseNamingStrategy() }
         };
         var camelCaseJson = JsonConvert.SerializeObject(testObject, camelCaseSettings);
         var camelCaseParsed = JObject.Parse(camelCaseJson);
-        foreach (var prop in propertyNames)
+        foreach (var prop in camelCaseParsed.Properties())
         {
-            var serializedProp = camelCaseParsed.Properties().FirstOrDefault(p => p.Value.ToString() == testObject.GetType().GetProperty(prop).GetValue(testObject).ToString());
-            results[prop]["Newtonsoft.Json (CamelCase)"] = serializedProp?.Name ?? "N/A";
+            var originalProp = propertyNames.FirstOrDefault(p => testObject.GetType().GetProperty(p).GetValue(testObject).ToString() == prop.Value.ToString());
+            if (originalProp != null)
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // Newtonsoft.Json (SnakeCase)
+        serializerName = "Newtonsoft.Json (SnakeCase)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var snakeCaseSettings = new JsonSerializerSettings
         {
             ContractResolver = new DefaultContractResolver { NamingStrategy = new SnakeCaseNamingStrategy() }
         };
         var snakeCaseJson = JsonConvert.SerializeObject(testObject, snakeCaseSettings);
         var snakeCaseParsed = JObject.Parse(snakeCaseJson);
-        foreach (var prop in propertyNames)
+        foreach (var prop in snakeCaseParsed.Properties())
         {
-            var serializedProp = snakeCaseParsed.Properties().FirstOrDefault(p => p.Value.ToString() == testObject.GetType().GetProperty(prop).GetValue(testObject).ToString());
-            results[prop]["Newtonsoft.Json (SnakeCase)"] = serializedProp?.Name ?? "N/A";
+            var originalProp = propertyNames.FirstOrDefault(p => testObject.GetType().GetProperty(p).GetValue(testObject).ToString() == prop.Value.ToString());
+            if (originalProp != null)
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // Newtonsoft.Json (KebabCase)
+        serializerName = "Newtonsoft.Json (KebabCase)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var kebabCaseSettings = new JsonSerializerSettings
         {
             ContractResolver = new DefaultContractResolver { NamingStrategy = new KebabCaseNamingStrategy() }
         };
         var kebabCaseJson = JsonConvert.SerializeObject(testObject, kebabCaseSettings);
         var kebabCaseParsed = JObject.Parse(kebabCaseJson);
-        foreach (var prop in propertyNames)
+        foreach (var prop in kebabCaseParsed.Properties())
         {
-            var serializedProp = kebabCaseParsed.Properties().FirstOrDefault(p => p.Value.ToString() == testObject.GetType().GetProperty(prop).GetValue(testObject).ToString());
-            results[prop]["Newtonsoft.Json (KebabCase)"] = serializedProp?.Name ?? "N/A";
+            var originalProp = propertyNames.FirstOrDefault(p => testObject.GetType().GetProperty(p).GetValue(testObject).ToString() == prop.Value.ToString());
+            if (originalProp != null)
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // System.Text.Json (Default)
-        var systemDefaultJson = System.Text.Json.JsonSerializer.Serialize(testObject);
+        serializerName = "System.Text.Json (Default)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
+        var systemDefaultJson = JsonSerializer.Serialize(testObject);
         var systemDefaultDoc = JsonDocument.Parse(systemDefaultJson);
         foreach (var prop in systemDefaultDoc.RootElement.EnumerateObject())
         {
             var originalProp = propertyNames.FirstOrDefault(p => prop.Value.GetString() == testObject.GetType().GetProperty(p).GetValue(testObject).ToString());
             if (originalProp != null)
-                results[originalProp]["System.Text.Json (Default)"] = prop.Name;
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // System.Text.Json (CamelCase)
+        serializerName = "System.Text.Json (CamelCase)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var systemCamelCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-        var systemCamelCaseJson = System.Text.Json.JsonSerializer.Serialize(testObject, systemCamelCaseOptions);
+        var systemCamelCaseJson = JsonSerializer.Serialize(testObject, systemCamelCaseOptions);
         var systemCamelCaseDoc = JsonDocument.Parse(systemCamelCaseJson);
         foreach (var prop in systemCamelCaseDoc.RootElement.EnumerateObject())
         {
             var originalProp = propertyNames.FirstOrDefault(p => prop.Value.GetString() == testObject.GetType().GetProperty(p).GetValue(testObject).ToString());
             if (originalProp != null)
-                results[originalProp]["System.Text.Json (CamelCase)"] = prop.Name;
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // System.Text.Json (SnakeCaseLower)
+        serializerName = "System.Text.Json (SnakeCaseLower)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var systemSnakeCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower };
-        var systemSnakeCaseJson = System.Text.Json.JsonSerializer.Serialize(testObject, systemSnakeCaseOptions);
+        var systemSnakeCaseJson = JsonSerializer.Serialize(testObject, systemSnakeCaseOptions);
         var systemSnakeCaseDoc = JsonDocument.Parse(systemSnakeCaseJson);
         foreach (var prop in systemSnakeCaseDoc.RootElement.EnumerateObject())
         {
             var originalProp = propertyNames.FirstOrDefault(p => prop.Value.GetString() == testObject.GetType().GetProperty(p).GetValue(testObject).ToString());
             if (originalProp != null)
-                results[originalProp]["System.Text.Json (SnakeCaseLower)"] = prop.Name;
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // System.Text.Json (KebabCaseLower)
+        serializerName = "System.Text.Json (KebabCaseLower)";
+        serializers.Add(serializerName);
+        results[serializerName] = new Dictionary<string, string>();
         var systemKebabCaseOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.KebabCaseLower };
-        var systemKebabCaseJson = System.Text.Json.JsonSerializer.Serialize(testObject, systemKebabCaseOptions);
+        var systemKebabCaseJson = JsonSerializer.Serialize(testObject, systemKebabCaseOptions);
         var systemKebabCaseDoc = JsonDocument.Parse(systemKebabCaseJson);
         foreach (var prop in systemKebabCaseDoc.RootElement.EnumerateObject())
         {
             var originalProp = propertyNames.FirstOrDefault(p => prop.Value.GetString() == testObject.GetType().GetProperty(p).GetValue(testObject).ToString());
             if (originalProp != null)
-                results[originalProp]["System.Text.Json (KebabCaseLower)"] = prop.Name;
+                results[serializerName][originalProp] = prop.Name;
         }
 
         // Output markdown table
-        Console.WriteLine("| Property Name | Newtonsoft.Json (Default) | Newtonsoft.Json (CamelCase) | Newtonsoft.Json (SnakeCase) | Newtonsoft.Json (KebabCase) | System.Text.Json (Default) | System.Text.Json (CamelCase) | System.Text.Json (SnakeCaseLower) | System.Text.Json (KebabCaseLower) |");
-        Console.WriteLine("|---------------|---------------------------|------------------------------|------------------------------|------------------------------|----------------------------|-------------------------------|-----------------------------------|-----------------------------------|");
+        Console.WriteLine("The following table shows how different JSON serializers handle common acronym properties:");
 
+        // Header row
+        Console.Write("| Serializer |");
         foreach (var prop in propertyNames)
         {
-            Console.Write($"| `{prop}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("Newtonsoft.Json (Default)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("Newtonsoft.Json (CamelCase)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("Newtonsoft.Json (SnakeCase)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("Newtonsoft.Json (KebabCase)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("System.Text.Json (Default)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("System.Text.Json (CamelCase)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("System.Text.Json (SnakeCaseLower)", "N/A")}` |");
-            Console.Write($" `{results[prop].GetValueOrDefault("System.Text.Json (KebabCaseLower)", "N/A")}` |");
+            Console.Write($" `{prop}` |");
+        }
+        Console.WriteLine();
+
+        // Separator row
+        Console.Write("| --------------------------------- |");
+        foreach (var prop in propertyNames)
+        {
+            Console.Write(" ----------- |");
+        }
+        Console.WriteLine();
+
+        // Data rows
+        foreach (var serializer in serializers)
+        {
+            Console.Write($"| {serializer} |");
+            foreach (var prop in propertyNames)
+            {
+                var value = results[serializer].GetValueOrDefault(prop, "N/A");
+                Console.Write($" `{value}` |");
+            }
             Console.WriteLine();
         }
     }
